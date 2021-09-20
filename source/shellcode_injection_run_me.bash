e4878346353dc4f1e5d9baa4d045a8d4066b4a38
Nelbren <nelbren@gmail.com>
2021-09-20 17:39:39 -0600

    fixes to search for start and end address, v0.0.4

--- source/shellcode_injection_run_me.bash
+++ source/shellcode_injection_run_me.bash
@@ -2,7 +2,7 @@
 #
 # shellcode_injection_run_me.bash
 #
-# v0.0.1 - 2021-09-20 - nelbren@nelbren.com
+# v0.0.2 - 2021-09-20 - nelbren@nelbren.com
 #
 
 vuln="./vuln"
@@ -29,7 +29,12 @@ cmd[4]="x/40x \$rsp"
 gdb --batch --quiet -ex "${cmd[0]}" -ex "${cmd[1]}" -ex "${cmd[2]}" \
     -ex "${cmd[3]}" -ex "${cmd[4]}" $vuln | tee -a $output
 
-begin=$(grep "0x41414141" $output | head -1 | cut -d":" -f1)
-end=$(grep "0x42424242" $output | tail -1 | cut -d":" -f1)
+bmark="0x41414141"
+emark="0x42424242"
+begin=$(grep "^0x" $output | grep "$bmark" | head -1 | cut -d":" -f1)
+end=$(grep "^0x" $output | grep "$emark" | tail -1 | cut -d":" -f1)
+
+echo -e "\n+BEGIN => ${begin}"
+echo -e "+->END => ${end}\n"
 
 ./shellcode_injection_find_rip.py $begin $end
